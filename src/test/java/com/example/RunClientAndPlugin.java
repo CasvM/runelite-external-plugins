737e1e921c1965537e1856f651f2b081f1af04b4
Cas van Mierlo <mail.casvm@gmail.com>
2020-09-29 01:12:24 +0200

    added license and changed the name of the main test file

--- src/test/java/com/example/RunClientAndPlugin.java
+++ src/test/java/com/example/RunClientAndPlugin.java
@@ -3,7 +3,7 @@ package com.example;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class RunClientAndPlugin
 {
 	public static void main(String[] args) throws Exception
 	{
